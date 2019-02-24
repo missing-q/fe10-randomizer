36748fd2abab2111c70b07c8fff68d718c4ea28e
Missing-q <ivybetterit@gmail.com>
2019-02-24 18:06:57 -0500

    More nonsense

--- classtest.py
+++ classtest.py
@@ -16,18 +16,34 @@ with open('Assets/JIDS.csv', 'r') as f:
 
 #JID row layout:
     #row[0] = JID_LABEL, to be printed out with the rest of the block information
-    #row[1] = Charblock starting address
+    #row[1] = Classblock starting address
     #row[2] = Label address, likely no program functionality
     #row[3] = Calculated pointer, likely no program functionality
     #row[4] = Hex string, deliminates beginning of charblock
     #row[5] = Block length, read this many bytes from the beginning of the block
+    #row[6] = Class tier, determines how bytes are read/grouped
 
 #okay, it's time y'allmst
 with open("Test-Files/FE10Data.cms.decompressed", "rb") as binary_file:
+    def first_tier(b):
+        pass
+
+    def second_tier(b):
+        pass
+
+    def third_tier(b):
+        pass
+
+    options = {
+        1: first_tier,
+        2: second_tier,
+        3: third_tier
+    }
+
     #Go to beginning of file
     binary_file.seek(0, 0)
     for b in JIDS:
-        #Grab charblock
+        #Grab classblock
         index = int(b[1], 16)
         #print(index)
         binary_file.seek(index,0)
@@ -37,4 +53,6 @@ with open("Test-Files/FE10Data.cms.decompressed", "rb") as binary_file:
 
         #grab class data, method depends on class tier
 
+        #call funct based on class type
+        options[b[6]](b)
         #print(format(charblock))
