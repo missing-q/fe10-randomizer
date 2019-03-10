01715a0f8f2143d4a58ecc368bec389bc7c7b549
Missing-q <ivybetterit@gmail.com>
2019-03-10 12:08:51 -0400

    More randomization nonsense

--- randomize.py
+++ randomize.py
@@ -7,14 +7,23 @@ import io
 import randomizedispos
 import randomizedata
 
+diff = ["c", "h", "n"]
 def randomizer(path, seed, args):
     character_index = randomizedata.randomizedata(path, seed, args)
-    randomizedispos.r_dispos(path + "/zmap/bmap0101/dispos_n.bin", args, character_index)
+
+    #randomize part 1
+    for i in range(1,12):
+        for d in diff:
+            randomizedispos.r_dispos(path + "/zmap/bmap01" + str(i).zfill(2) + "/dispos_" + d + ".bin", args, character_index)
 
     #walk through every dispos file in dir
+    '''
     for subdir, dirs, files in os.walk(path):
         for file in files:
             #print os.path.join(subdir, file)
             filepath = subdir + os.sep + file
             if filepath.endswith(".bin"):
-                randomizedispos.r_dispos(filepath, args, character_index)
+                print(filepath)
+                #randomizedispos.r_dispos(filepath, args, character_index)
+
+    '''
--- randomizedispos.py
+++ randomizedispos.py
@@ -1,4 +1,6 @@
 def r_dispos(filename, args, index):
+    print(filename)
+    print("----------------------")
     #REMEMBER- when appending new labels to the end of a file, be sure to update the first 4 bytes in the file header with the new file length.
     import csv
     import binascii
